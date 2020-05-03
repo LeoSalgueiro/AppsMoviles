8a0489337b9aa4fe1bd0079d0a95596e9a3a364e
Federico Montes de Oca <fmontesdeoca@scorf.io>
2020-05-03 21:01:02 +0200

    Feat: dynamic data fetch. cart first version

--- practica2/js/main.js
+++ practica2/js/main.js
@@ -1,33 +1,3 @@
 /**
  * Here goes the js and jquery main code.
  */
-const hideAll = () => {
-  $(".section").each(function () {
-    $(this).addClass("hidden");
-  });
-};
-
-const fetchData = (games) => {
-  $.get(
-    "http://www.json-generator.com/api/json/get/clqOukMfVK?indent=2",
-    function (data) {
-      games.push(...data);
-    }
-  );
-};
-
-// init
-let games = [];
-fetchData(games);
-hideAll();
-
-// ready
-$(document).ready(function () {
-  console.log(games);
-
-  $(".cat-selection").click(function () {
-    hideAll();
-    // show
-    $(`#${$(this).attr("id")}-section`).removeClass("hidden");
-  });
-});
