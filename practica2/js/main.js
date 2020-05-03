480431909f4e58da7db0fdbd8b1cb8cbca2fe27d
Federico Montes de Oca <fmontesdeoca@scorf.io>
2020-05-03 23:17:51 +0200

    Minor fix: testing another path

--- practica2/js/main.js
+++ practica2/js/main.js
@@ -119,7 +119,7 @@ const fetchData = (url) => {
     console.log(
       "Fetching with local data from data/games.json because of 'GitHubPages non-https' resource error"
     );
-    $.getJSON("practica2/data/games.json", function (data) {
+    $.getJSON("../data/games.json", function (data) {
       buildArticles(data);
     });
   });
