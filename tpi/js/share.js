b91145004fa99262fe4d2b74ded95c908b8433d1
Federico Montes de Oca <fmontesdeoca@scorf.io>
2020-05-14 15:26:42 +0200

    Feat: share validation and css

--- tpi/js/share.js
+++ tpi/js/share.js
@@ -1,4 +1,43 @@
 $("#destinatary").on("change paste keyup", function () {
-  const destinatary = $("#destinatary").val();
-  $("form").attr("action", `${`mailto:${destinatary}`}`);
+  if (validateField($(this))) {
+    $("form").attr("action", `mailto:${$(this).val()}`);
+  }
+  submitBtnEnabled();
 });
+
+$("#subject").on("change paste keyup", function () {
+  validateField($(this));
+  submitBtnEnabled();
+});
+
+$("#body").on("change paste keyup", function () {
+  validateField($(this));
+  submitBtnEnabled();
+});
+
+$(document).ready(function () {
+  $("#submit").attr("disabled", true);
+});
+
+const validateField = (target) => {
+  let passes = false;
+  if (target.val().length < 1) {
+    target.parent().find(".error-message").removeClass("hidden");
+  } else {
+    target.parent().find(".error-message").addClass("hidden");
+    passes = true;
+  }
+  return passes;
+};
+
+const submitBtnEnabled = () => {
+  if (
+    $("#destinatary").val().length &&
+    $("#subject").val().length &&
+    $("#body").val().length
+  ) {
+    $("#submit").removeAttr("disabled");
+  } else {
+    $("#submit").attr("disabled", true);
+  }
+};
