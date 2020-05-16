277b4d628334752b348ef848ed49be2dccb764e3
Federico Montes de Oca <fmontesdeoca@scorf.io>
2020-05-16 08:59:07 +0200

    Feat: fetch api data for a country w/ filters

--- tpi/js/api.wrapper.js
+++ tpi/js/api.wrapper.js
@@ -17,3 +17,31 @@ const getAllAvailableCountries = $.ajax({
 });
 
 const getAllAvailableCountriesAttributes = ["Country", "Slug", "ISO2"];
+
+const buildGetCountryDataUrl = (
+  countrySlug,
+  status = "all",
+  from = undefined,
+  to = undefined,
+) => {
+  // build url
+  let build = `${baseUrl}/country/${countrySlug}`;
+  if (status != "all") {
+    build += `/status/${status}`;
+  }
+  if (from) {
+    build += `?from=${from}`;
+  }
+  if (to) {
+    build += from ? `&to=${to}` : `?to=${to}`;
+  }
+  return build;
+};
+
+const getCountryData = (url) => {
+  return $.ajax({
+    url,
+    method: "GET",
+    timeout: 0,
+  });
+};
--- tpi/js/main.js
+++ tpi/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+  jQuery(".datetimepicker").datetimepicker();
   showOnlyAmongBrothers($("#summary-section"));
   getSummary.done((data) => {
     console.log(data);
@@ -21,22 +22,57 @@ $(function () {
     const input = $("#search-bar-input").val();
     if (input.length) {
       getAllAvailableCountries.done((data) => {
-        let match = false;
+        let match = null;
         for (const country of data) {
           for (const attr of getAllAvailableCountriesAttributes) {
             if (country[attr].includes(input)) {
               alert(JSON.stringify(country));
-
-              // first found.
-              match = true;
+              match = country;
+              // first found is the result.
               break;
             }
           }
         }
         if (!match) {
-          alert("no results for: " + input);
+          // TODO meterlo en un div
+          alert("No se encontraron resultados para: " + input);
+        } else {
+          // trigger request to fetch country data with filters
+          const filters = getFiltersValues();
+
+          // TODO save on localstorage with filters
+          const searchObject = {
+            match,
+            filters,
+            timestamp: moment().format("YYYY-MM-DDThh:mm:00.000") + "Z",
+          };
+          console.log(filters);
+          getCountryData(
+            buildGetCountryDataUrl(
+              match.Slug,
+              filters.status,
+              filters.from,
+              filters.to,
+            ),
+          ).done((data) => {
+            console.log(data);
+          });
         }
       });
     }
   });
 });
+
+const getFiltersValues = () => {
+  return {
+    status: $("#filter-status").find(":selected").val(),
+    from:
+      moment($("#filter-from").val(), "YYYY-MM-DD hh:mm").format(
+        "YYYY-MM-DDThh:mm:00.000",
+      ) + "Z",
+    to:
+      moment($("#filter-to").val(), "YYYY-MM-DD hh:mm").format(
+        "YYYY-MM-DDThh:mm:00.000",
+      ) + "Z",
+  };
+};
