5b9c908432dc91522777946f06b40057b366adcd
Leonel <dismoi.leo@gmail.com>
2020-05-16 22:31:54 -0300

    Obtener pais (imagen de bandera y de pais terminado)

--- tpi/js/consumerOfImages.js
+++ tpi/js/consumerOfImages.js
@@ -1,8 +1,7 @@
 
 const countryForCode = 'https://restcountries.eu/rest/v2/currency/'; //+el codigo de pais de tres digitos
 
-const wallHavenSearch = 'https://wallhaven.cc/api/v1/search?q='; //+ la capital del pais
-
+const pixabayParte1 = 'https://pixabay.com/api/?key=16581745-b1d7d985385c1702b85b0971b&image_type=photo&category=places&min_width=2500&min_height=2500&q='; //+ la capital del pais
 let infoGeneral = {
     ISO2: "",
     flag: "",
@@ -13,8 +12,6 @@ function getCountryImages(ISO2, callback){
     
     infoGeneral.ISO2 = ISO2;
     getCountry(ISO2); //traigo la imagen del country
-
-    console.log(infoGeneral)
     //callback(infoGeneral)
 }
 
@@ -25,15 +22,16 @@ const getCountry = (iso2) => $.ajax({
     url: `${countryForCode}`+iso2,
     method: "GET",
     timeout: 0,
-    success: function(response){
+    success: async function(response){
         infoGeneral.flag = response[0].flag
-        
+        await getImagen(response[0].capital)
+        //console.log(infoGeneral) ver resultado en la consola
     }
     
 });
 
 const getImagen = (capital) => $.ajax({
-    url: `${wallHavenSearch}`+capital,
+    url: `${pixabayParte1}`+capital,
     method: "GET",
     timeout: 0,
     
@@ -44,7 +42,14 @@ const getImagen = (capital) => $.ajax({
     dataType: "json",
     
     success: function(response){
-        console.log(response[0])
+        console.log(response.hits[0].largeImageURL)
+        if(typeof(response.hits[0]) === 'undefined'){
+            infoGeneral.image = "https://i.pinimg.com/originals/93/e3/97/93e3971ac2d293b8270fd045a3b78b99.jpg"; //si no hay ninguna imagen para poner, pone por defecto la de la app (podria ser la bandera del pais)
+        }
+        else{
+            infoGeneral.image = response.hits[0].largeImageURL
+        }
+        
     },
     error: function () {
         console.log("error");
