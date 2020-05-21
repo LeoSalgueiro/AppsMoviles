707274ef71d7f27c78becee4b5ca8d4db67d82c8
Federico Montes de Oca <fmontesdeoca@scorf.io>
2020-05-21 02:20:37 +0200

    Fix: on flag request fail, show the article without flag.

--- tpi/js/elementBuilders.js
+++ tpi/js/elementBuilders.js
@@ -118,13 +118,22 @@ const buildEmptyHistory = (target) => {
 };
 
 const buildSearchResultItem = (target, countryName) => {
-  getCountryFlag(countryName.ISO2).done(function (res) {
-    target.append(`
+  getCountryFlag(countryName.ISO2)
+    .done(function (res) {
+      target.append(`
     <article id="search-list-country-${countryName.Country}_${countryName.Slug}_${countryName.ISO2}" 
         class="row card background-white hoverable country-item" onclick="presentCountryData('${countryName.Country}', '${countryName.Slug}', '${countryName.ISO2}')">
       <span class="col"><img src="${res.flag}" alt="country" style="width: 32px" /></span>
       <span class="col">${countryName.Country}</span>
     </article>
     `);
-  });
+    })
+    .fail(function () {
+      target.append(`
+    <article id="search-list-country-${countryName.Country}_${countryName.Slug}_${countryName.ISO2}" 
+        class="row card background-white hoverable country-item" onclick="presentCountryData('${countryName.Country}', '${countryName.Slug}', '${countryName.ISO2}')">
+      <span class="col">${countryName.Country}</span>
+    </article>
+    `);
+    });
 };
